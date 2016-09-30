36c5ec4c4785a76cb5ed71b54fa282aa436c0e3b
tainavm <tainaviriato@gmail.com>
2016-09-29 23:36:27 -0300

    função de abrir a galeria e botões na tela

--- app/src/main/java/com/rafaelguimas/pid/MainActivity.java
+++ app/src/main/java/com/rafaelguimas/pid/MainActivity.java
@@ -1,30 +1,31 @@
 package com.rafaelguimas.pid;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMG = 1 ;
     public static String TAG = "MainActivity";
+    Button btnSelect1, btnSelect2 , btnAnd;
 
     static {
         if (OpenCVLoader.initDebug()) {
@@ -34,13 +35,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.img3);
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+            } else {
+                Toast.makeText(this, "Selecione uma imagem",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnSelect1 =  (Button) findViewById(R.id.btnSelect1);
+        Button btnAnd =  (Button) findViewById(R.id.btnSelect1);
+        Button btnSelect2 =  (Button) findViewById(R.id.btnSelect1);
+
+        btnSelect1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+            }
+        });
+
+
+
+
         TextView txt1 = (TextView) findViewById(R.id.txt1);
-        TextView txt2 = (TextView) findViewById(R.id.txt2);
+     //   TextView txt2 = (TextView) findViewById(R.id.txt2);
         TextView txt3 = (TextView) findViewById(R.id.txt3);
 
         // make a mat and draw something
