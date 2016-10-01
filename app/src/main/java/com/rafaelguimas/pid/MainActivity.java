404616c8c8120a66116926925f1d2abc154c5503
Rafael Guimaraes <rafaellguimas@gmail.com>
2016-10-01 13:58:48 -0300

    Refatoração da tela principal; Início da implementação de seleção de imagens da galera (falta permissão); Execução da operação XOR;

--- app/src/main/java/com/rafaelguimas/pid/MainActivity.java
+++ app/src/main/java/com/rafaelguimas/pid/MainActivity.java
@@ -9,23 +9,37 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RESULT_LOAD_IMG = 1 ;
     public static String TAG = "MainActivity";
-    Button btnSelect1, btnSelect2 , btnAnd;
+    public static int RESULT_LOAD_IMAGE_1 = 1;
+    public static int RESULT_LOAD_IMAGE_2 = 2;
+
+    private Bitmap img1, img2;
+    private Mat mat1, mat2, mat3;
+
+    private ImageView iv, iv2, iv3;
 
     static {
         if (OpenCVLoader.initDebug()) {
@@ -35,97 +49,100 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            // When an Image is picked
-            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
-                    && null != data) {
-                // Get the Image from data
-
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                // Move to first row
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                String imgDecodableString = cursor.getString(columnIndex);
-                cursor.close();
-                ImageView imgView = (ImageView) findViewById(R.id.img3);
-                // Set the Image in ImageView after decoding the String
-                imgView.setImageBitmap(BitmapFactory
-                        .decodeFile(imgDecodableString));
-
-            } else {
-                Toast.makeText(this, "Selecione uma imagem",
-                        Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                    .show();
-        }
-
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSelect1 =  (Button) findViewById(R.id.btnSelect1);
-        Button btnAnd =  (Button) findViewById(R.id.btnSelect1);
-        Button btnSelect2 =  (Button) findViewById(R.id.btnSelect1);
+        // Objetos da tela
+        iv = (ImageView) findViewById(R.id.img1);
+        iv2 = (ImageView) findViewById(R.id.img2);
+        iv3 = (ImageView) findViewById(R.id.img3);
+        Button btnSelect1 = (Button) findViewById(R.id.btnSelect1);
+        Button btnSelect2 = (Button) findViewById(R.id.btnSelect2);
+        Button btnOperation = (Button) findViewById(R.id.btnOperation);
 
         btnSelect1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE_1);
             }
         });
 
+        btnSelect2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE_2);
+            }
+        });
 
-
-
-        TextView txt1 = (TextView) findViewById(R.id.txt1);
-     //   TextView txt2 = (TextView) findViewById(R.id.txt2);
-        TextView txt3 = (TextView) findViewById(R.id.txt3);
-
-        // make a mat and draw something
+        // Exemplo de criacao de matriz
 //        Mat m = new Mat(100, 400, CvType.CV_8UC3);
 
-        Mat mat1 = null;
-        Mat mat2 = null;
+        // Cria as matrizes com os drawables
         try {
             mat1 = Utils.loadResource(this, R.drawable.lena_gray);
             mat2 = Utils.loadResource(this, R.drawable.lena_gray_dot);
+            mat3 = Utils.loadResource(this, R.drawable.lena_gray);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        Imgproc.cvtColor(mat1, mat1, Imgproc.COLOR_BGR2GRAY);
-        Mat mat3 = new Mat();
-        Core.bitwise_xor(mat1, mat2, mat1);
-
         // Converte mat para bitmap
-        Bitmap bm = Bitmap.createBitmap(mat1.cols(), mat1.rows(), Bitmap.Config.ARGB_8888);
+        Bitmap bm1 = Bitmap.createBitmap(mat1.cols(), mat1.rows(), Bitmap.Config.ARGB_8888);
         Bitmap bm2 = Bitmap.createBitmap(mat2.cols(), mat2.rows(), Bitmap.Config.ARGB_8888);
-//        Bitmap bm3 = Bitmap.createBitmap(mat3.cols(), mat3.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(mat1, bm);
+        Utils.matToBitmap(mat1, bm1);
         Utils.matToBitmap(mat2, bm2);
 
-        // Coloca no imageview
-        ImageView iv = (ImageView) findViewById(R.id.img1);
-        ImageView iv2 = (ImageView) findViewById(R.id.img2);
-        ImageView iv3 = (ImageView) findViewById(R.id.img2);
-        iv.setImageBitmap(bm);
+        // Exibe as imagens de entrada
+        iv.setImageBitmap(bm1);
         iv2.setImageBitmap(bm2);
-//        iv3.setImageBitmap(bm3);
 
+        // Clique do botao de operacao
+        btnOperation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Executa operacoes com as matrizes
+//                Imgproc.cvtColor(mat1, mat1, Imgproc.COLOR_BGR2GRAY);
+                Core.bitwise_xor(mat3, mat2, mat3);
+
+                // Converte o resultado para bm
+                Bitmap bm3 = Bitmap.createBitmap(mat3.cols(), mat3.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(mat3, bm3);
+
+                // Exibe a imagem do resultado
+                iv3.setImageBitmap(bm3);
+
+                // Alerta do que deve ser desenvolvido
+                Toast.makeText(getApplicationContext(), "Abre o dialog e seleciona operacao (padrão: xor)", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Codigo comum pras duas imagens
+        String picturePath = "";
+        if (resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+        }
+
+        if (requestCode == RESULT_LOAD_IMAGE_1) {
+            img1 = BitmapFactory.decodeFile(picturePath);
+            iv.setImageBitmap(img1);
+        } else if (requestCode == RESULT_LOAD_IMAGE_2) {
+            img2 = BitmapFactory.decodeFile(picturePath);
+            iv2.setImageBitmap(img2);
+        }
     }
 }
