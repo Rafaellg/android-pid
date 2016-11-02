d965f1cb67f2eeae1f5776a948df86bdc4bedf9b
Rafael Guimaraes <rafaellguimas@gmail.com>
2016-11-02 12:49:16 -0200

    Criação da activity de moedas;

---
+++ app/src/main/java/com/rafaelguimas/pid/CoinFragment.java
@@ -0,0 +1,29 @@
+package com.rafaelguimas.pid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CoinFragment extends Fragment {
+
+    public CoinFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_coin, container, false);
+
+
+        return view;
+    }
+
+}
